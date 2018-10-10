67b763185f666a7f96fa79a6e25ee8467f05ff80
Ewelina Brus <idvarnew@gmail.com>
2018-10-10 04:44:51 +0200

    Add new reducers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { UIState } from './store/reducers/uiState.reducer';
 import { DataState } from './store/reducers/dataState.reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { Effects } from './store/app.effects';
 import { AppComponent } from './app.component';
-import {FontdetailsModule} from './modules/font-details/font-details.module';
-import {FontsModule} from './modules/fonts/fonts.module';
+import { FontdetailsModule } from './modules/font-details/font-details.module';
+import { FontsModule } from './modules/fonts/fonts.module';
 import { SelectedFontsModule } from './modules/selected-fonts/selected-fonts.module';
 import { CoreModule } from './core/core.module';
+import { FilterFonts } from './store/reducers/filterFonts.reducer';
+import { CacheFonts } from './store/reducers/cachedFonts.reducer';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     NgbModule.forRoot(),
     BrowserModule,
@@ -28,12 +28,14 @@ import { CoreModule } from './core/core.module';
     StoreModule.forRoot({
       dataState: DataState,
       uiState: UIState,
+      filterFonts: FilterFonts,
+      cacheFonts: CacheFonts
     }),
-  FontdetailsModule,
-   FontsModule,
-    SelectedFontsModule,
+    FontdetailsModule,
+    FontsModule,
+    SelectedFontsModule
   ],
-  providers: [  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
